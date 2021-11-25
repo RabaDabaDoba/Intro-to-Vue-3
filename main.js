e24fb72c2061226cdff69f9b8e2bf01ec6f74cc0
RabaDabaDoba <rodah1997@gmail.com>
2021-11-25 11:26:11 +0100

    Done

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
+            images: ['./assets/images/socks_blue.jpg', './assets/images/socks_green.jpg'],
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+
+        },
+        changeColor(color) {
+            if (color == this.variants[0].color) {
+                this.image = this.variants[0].image
+            } else {
+                this.image = this.variants[1].image
+            }
+        },
+        changeColorV2(image) {
+            this.image = image
         }
     }
 })
