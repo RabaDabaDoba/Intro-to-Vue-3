38af5898fb50cc7a3132b7863dbf2b13dff37178
RabaDabaDoba <rodah1997@gmail.com>
2021-11-25 14:36:49 +0100

    Got it!

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,17 +1,23 @@
 // solution
 app.component('product-details', {
-  props: {
-    details: {
-      type: Array,
-      required: true
-    }
-  },
-  template:
-  /*html*/
-  `
+        props: {
+            details: {
+                type: Array,
+                required: true
+            },
+            asd: {
+                type: Number,
+                required: true
+            }
+        },
+        template:
+        /*html*/
+            `
+            
   <ul>
     <li v-for="detail in details">{{ detail }}</li>
   </ul>
+  <p>{{asd}}</p>
   `
-})
-// solution
+    })
+    // solution
