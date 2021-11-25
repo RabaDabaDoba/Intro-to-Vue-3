9a677cfc6902cf63037bc6f735ccfc851fbbaade
RabaDabaDoba <rodah1997@gmail.com>
2021-11-25 10:08:05 +0100

    Done

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'http://mio.se',
+        }
+    },
+
+    methods: {
+        changePic() {
+            var blue = './assets/images/socks_blue.jpg'
+            var green = './assets/images/socks_green.jpg'
+            if (this.image === blue) {
+                this.image = green
+            } else {
+                this.image = blue
+            }
         }
     }
 })
