de10ff0a07648b0a9278f9fc89144b6548a91263
RabaDabaDoba <rodah1997@gmail.com>
2021-11-25 10:18:37 +0100

    Done
    Added a buy method that triggers the conditional rendering.

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 11,
+        }
+    },
+    methods: {
+        buy() {
+            if (this.inventory > 0) {
+                this.inventory--;
+            } else {
+                alert('Out of stock!')
+            }
+
         }
     }
+
 })
